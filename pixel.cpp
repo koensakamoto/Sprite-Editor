5883540d8a164cf1f8d9e9315583fdf1b48534f7
joshThalathoty <jtthalathoty@gmail.com>
2025-03-22 21:52:17 -0600

    reordering variables into RGBA

--- pixel.cpp
+++ pixel.cpp
@@ -1,17 +1,17 @@
 #include "pixel.h"
 
-Pixel::Pixel(): red(0), blue(0), green(0), alpha(0), colored(false) {}
+Pixel::Pixel(): red(0), green(0), blue(0), alpha(0), colored(false) {}
 
 void Pixel::erasePixel(){
     red = 0;
-    blue = 0;
     green = 0;
+    blue = 0;
     alpha = 0;
     colored = false;
 
 }
 
-void Pixel::setPixel(unsigned char r, unsigned char b, unsigned char g, unsigned char a){
+void Pixel::setPixel(unsigned char r, unsigned char g, unsigned char b,  unsigned char a){
     red = r;
     blue = b;
     green = g;
--- pixel.h
+++ pixel.h
@@ -10,8 +10,8 @@ private:
 
     // red, green and blue values ranging from 0 to 255.
     unsigned char red;
-    unsigned char blue;
     unsigned char green;
+    unsigned char blue;
 
     // transparency
     unsigned char alpha;
@@ -21,17 +21,28 @@ private:
 
 public:
 
-    // Default constructor sets numerical values to 0, and isColored to false.
+    /**
+     * @brief Pixel Default constructor.
+     */
     Pixel();
+
+    /**
+     * @brief Pixel creates a Pixel object with the specified RGBA values.
+     * @param r Value from 0-255 representing the red value of a pixel.
+     * @param g Value from 0-255 representing the green value of a pixel.
+     * @param b Value from 0-255 representing the blue value of a pixel.
+     * @param a Value from 0-255 representing the alpha value of a pixel.
+     */
+    Pixel(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 
     /**
      * @brief setPixel Sets the RGBA values of a pixel, and isColored to true.
      * @param r Value from 0-255 representing the red value of a pixel.
-     * @param b Value from 0-255 representing the blue value of a pixel.
      * @param g Value from 0-255 representing the green value of a pixel.
+     * @param b Value from 0-255 representing the blue value of a pixel.
      * @param a Value from 0-255 representing the alpha value of a pixel.
      */
-    void setPixel(unsigned char r, unsigned char b, unsigned char g, unsigned char a);
+    void setPixel(unsigned char r, unsigned char g, unsigned char b,  unsigned char a);
 
     /**
      * @brief erasePixel Sets isColored to false. Deletes previous color information by
